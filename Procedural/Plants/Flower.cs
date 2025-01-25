389b3cf2bd4565cf6f266badd2a7cb8b946a282c
hal-9001-v <vmag2001minus1@hotmail.com>
2025-01-25 17:41:05 +0100

    Girar flor

--- Procedural/Plants/Flower.cs
+++ Procedural/Plants/Flower.cs
@@ -3,26 +3,33 @@ using UnityEngine;
 
 public class Flower : MonoBehaviour
 {
-    [Range(0, 20)] public int petals;
+	[Range(0, 20)] public int petals;
 
-    [Range(0, 1)] public float separation;
-    public LeafDNA2 dna;
+	[Range(0, 1)] public float separation;
+	public LeafDNA2 dna;
 
-    private MeshFilter MeshFilter => GetComponent<MeshFilter>();
-    private MeshRenderer MeshRenderer => GetComponent<MeshRenderer>();
+	private MeshFilter MeshFilter => GetComponent<MeshFilter>();
+	private MeshRenderer MeshRenderer => GetComponent<MeshRenderer>();
 
-    private float elapsedTime = 0;
+	private float elapsedTime = 0;
 
-    private void Update()
-    {
-        elapsedTime += Time.deltaTime;
-        if (elapsedTime < 0.5f)
-        {
-            elapsedTime += Time.deltaTime;
-            return;
-        }
-        var mesh = LeafGeometry.GetFlowerPetalsMesh(dna, petals, separation, Vector3.zero, Vector3.zero, out var propertyBlock);
-        MeshFilter.sharedMesh = mesh;
-        MeshRenderer.SetPropertyBlock(propertyBlock);
-    }
+	public Vector3 face;
+
+	private void Update()
+	{
+		elapsedTime += Time.deltaTime;
+		if (elapsedTime < 0.5f)
+		{
+			elapsedTime += Time.deltaTime;
+			return;
+		}
+		var mesh = LeafGeometry.GetFlowerPetalsMesh(dna, petals, separation, Vector3.zero, face, out var propertyBlock);
+		MeshFilter.sharedMesh = mesh;
+		MeshRenderer.SetPropertyBlock(propertyBlock);
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.DrawLine(transform.position, transform.position + face.normalized * 100);
+	}
 }
